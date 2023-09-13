f9f94b1fc1a4a2e7b017d0b84f2aa91c6f64f92d
JayCode Developer <JayCode.contakt@gmail.com>
2023-09-14 01:22:46 +0200

    Style Carousel for full Screen

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { AnimatePresence, motion, MotionConfig } from "framer-motion";
 import useKeypress from "react-use-keypress";
+import { atom } from "nanostores";
 
 interface CarouselProps {
   images: any;
@@ -29,95 +30,107 @@ export default function Carousel({ images, runIndex }: CarouselProps) {
 
   return (
     <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
-      <div className="max-h-7xl bg-black">
-        <div className="mx-auto flex h-full max-w-7xl flex-col justify-center">
-          <div className="relative overflow-hidden">
-            <motion.div
-              animate={{ x: `-${index * 100}%` }}
-              className="flex  aspect-[3/2]"
-            >
-              {images.map((image, i) => (
-                <motion.img
-                  key={image.src}
-                  src={image.src}
-                  animate={{ opacity: i === index ? 1 : 0.3 }}
-                  className="aspect-[3/2] object-contain "
-                />
-              ))}
-            </motion.div>
-            <AnimatePresence initial={false}>
-              {index > 0 && (
-                <motion.button
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 0.7 }}
-                  exit={{ opacity: 0, pointerEvents: "none" }}
-                  whileHover={{ opacity: 1 }}
-                  className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
-                  onClick={() => setIndex(index - 1)}
-                >
-                  Left
-                </motion.button>
-              )}
-            </AnimatePresence>
-
-            <AnimatePresence initial={false}>
-              {index + 1 < images.length && (
-                <motion.button
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 0.7 }}
-                  exit={{ opacity: 0, pointerEvents: "none" }}
-                  whileHover={{ opacity: 1 }}
-                  className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
-                  onClick={() => setIndex(index + 1)}
-                >
-                  Right
-                </motion.button>
-              )}
-            </AnimatePresence>
-          </div>
-
-          <div className="absolute inset-x-0 bottom-6 flex justify-center overflow-hidden">
-            <motion.div
-              initial={false}
-              animate={{
-                x: `-${
-                  index * 100 * (collapsedAspectRatio / fullAspectRatio) +
-                  index * gap +
-                  margin
-                }%`,
-              }}
-              style={{ aspectRatio: fullAspectRatio, gap: `${gap}%` }}
-              className="flex h-14"
-            >
-              {images.map((image, i) => (
-                <motion.button
-                  key={image}
-                  onClick={() => setIndex(i)}
-                  whileHover={{ opacity: 1 }}
-                  initial={false}
-                  animate={i === index ? "active" : "inactive"}
-                  variants={{
-                    active: {
-                      marginLeft: `${margin}%`,
-                      marginRight: `${margin}%`,
-                      opacity: 1,
-                      aspectRatio: fullAspectRatio,
-                    },
-                    inactive: {
-                      marginLeft: "0%",
-                      marginRight: "0%",
-                      opacity: 0.5,
-                      aspectRatio: collapsedAspectRatio,
-                    },
-                  }}
-                >
+      <div className="fixed w-full h-full top-0 z-40 left-0 bg-black opacity-70"></div>
+      <div
+        className="fixed w-full h-full top-0 left-0 z-50 flex justify-center items-center"
+        onClick={() => console.log("Close")}
+      >
+        <button
+          className="text-light z-60 fixed top-10 right-10"
+          onClick={() => console.log("Close")}
+        >
+          Close
+        </button>
+        <div className="max-w-7xl bg-black z-60">
+          <div className="mx-auto flex h-full max-w-7xl flex-col justify-center">
+            <div className="relative overflow-hidden">
+              <motion.div
+                animate={{ x: `-${index * 100}%` }}
+                className="flex  aspect-[3/2]"
+              >
+                {images.map((image, i) => (
                   <motion.img
+                    key={image.src}
                     src={image.src}
-                    className="h-full object-cover w-full"
+                    animate={{ opacity: i === index ? 1 : 0.3 }}
+                    className="aspect-[3/2] object-contain "
                   />
-                </motion.button>
-              ))}
-            </motion.div>
+                ))}
+              </motion.div>
+              <AnimatePresence initial={false}>
+                {index > 0 && (
+                  <motion.button
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 0.7 }}
+                    exit={{ opacity: 0, pointerEvents: "none" }}
+                    whileHover={{ opacity: 1 }}
+                    className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
+                    onClick={() => setIndex(index - 1)}
+                  >
+                    Left
+                  </motion.button>
+                )}
+              </AnimatePresence>
+
+              <AnimatePresence initial={false}>
+                {index + 1 < images.length && (
+                  <motion.button
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 0.7 }}
+                    exit={{ opacity: 0, pointerEvents: "none" }}
+                    whileHover={{ opacity: 1 }}
+                    className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
+                    onClick={() => setIndex(index + 1)}
+                  >
+                    Right
+                  </motion.button>
+                )}
+              </AnimatePresence>
+            </div>
+
+            <div className="absolute inset-x-0 bottom-6 flex justify-center overflow-hidden">
+              <motion.div
+                initial={false}
+                animate={{
+                  x: `-${
+                    index * 100 * (collapsedAspectRatio / fullAspectRatio) +
+                    index * gap +
+                    margin
+                  }%`,
+                }}
+                style={{ aspectRatio: fullAspectRatio, gap: `${gap}%` }}
+                className="flex h-14"
+              >
+                {images.map((image, i) => (
+                  <motion.button
+                    key={image}
+                    onClick={() => setIndex(i)}
+                    whileHover={{ opacity: 1 }}
+                    initial={false}
+                    animate={i === index ? "active" : "inactive"}
+                    variants={{
+                      active: {
+                        marginLeft: `${margin}%`,
+                        marginRight: `${margin}%`,
+                        opacity: 1,
+                        aspectRatio: fullAspectRatio,
+                      },
+                      inactive: {
+                        marginLeft: "0%",
+                        marginRight: "0%",
+                        opacity: 0.5,
+                        aspectRatio: collapsedAspectRatio,
+                      },
+                    }}
+                  >
+                    <motion.img
+                      src={image.src}
+                      className="h-full object-cover w-full"
+                    />
+                  </motion.button>
+                ))}
+              </motion.div>
+            </div>
           </div>
         </div>
       </div>
