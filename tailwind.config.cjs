b4e2b72953b963e72c32e1cb9a5c7d3796df66ef
JayCode Developer <JayCode.contakt@gmail.com>
2023-11-02 11:57:37 +0100

    Add Intro animation to Instruktorzy page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,21 @@ module.exports = {
 				'primaryHover': '#B90B14',
 				'dark': '#181819',
 				'light': '#FFFDED'
-			}
+			},
+			keyframes: {
+				in: {
+				  "0%": { transform: "translateY(18px)", opacity: 0 },
+				  "100%": { transform: "translateY(0)", opacity: 1 },
+				},
+				"in-reverse": {
+				  "0%": { transform: "translateY(-18px)", opacity: 0 },
+				  "100%": { transform: "translateY(0px)", opacity: 1 },
+				},
+			  },
+			  animation: {
+				in: "in .6s both",
+				"in-reverse": "in-reverse .6s both",
+			  },
 		},
 	},
 	plugins: [],
